def659d3607eba41ed4d0841a711b5afaa98f886
Toby Qin <toby.qin@live.com>
2017-06-01 09:56:30 +0800

    add static root.

--- testcube/settings.py
+++ testcube/settings.py
@@ -117,5 +117,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(SETTINGS_DIR, "static")
+    os.path.join(SETTINGS_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
