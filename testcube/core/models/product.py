47c104e2f76683af9aaff2ef1401d06802ba9c22
Toby Qin <toby.qin@live.com>
2017-06-30 18:18:54 +0800

    model update: add team to product.

--- testcube/core/models/product.py
+++ testcube/core/models/product.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from .team import Team
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     version = models.CharField(max_length=20, default='latest')
     owner = models.CharField(max_length=50, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='products')
 
     class Meta:
         ordering = ['id']
