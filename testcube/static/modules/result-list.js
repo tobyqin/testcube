a3225e28a7800f602c3d3af00bc78754313b1c31
Toby Qin <toby.qin@live.com>
2017-09-11 16:07:58 +0800

    refactor js for result list page.

--- testcube/static/modules/result-list.js
+++ testcube/static/modules/result-list.js
@@ -1,3 +1,40 @@
-/**
- * Created by tobyqin on 10/09/2017.
- */
+define(['jquery', './table-support'],
+    function ($, support) {
+
+        "use strict";
+        let formatter = support.formatter;
+
+        let resultListColumns = [
+            {title: 'ID', field: 'id', formatter: formatter.resultIdFormatter, sortable: true},
+            {title: 'TestCase', field: 'testcase_name'},
+            {title: 'Message', field: 'error_message'},
+            {title: 'Created On', field: 'created_on', formatter: formatter.timeFormatter},
+            {title: 'Duration', field: 'duration', formatter: formatter.durationFormatter},
+            {title: 'Outcome', field: 'get_outcome_display', formatter: formatter.outcomeFormatter}
+        ];
+
+        function resultListTableRender(url) {
+            $('#table').bootstrapTable({
+                sidePagination: 'server',
+                url: url,
+                responseHandler: function (data) {
+                    data.total = data.count;
+                    data.rows = data.results;
+                    return data;
+                },
+                queryParams: support.refineQueryParams,
+                search: true,
+                pagination: true,
+                pageSize: 20,
+                pageList: [20, 50, 100],
+                sortName: 'id',
+                sortOrder: 'desc',
+                columns: resultListColumns
+            });
+        }
+
+        return {
+            resultListTableRender: resultListTableRender
+        };
+
+    });
