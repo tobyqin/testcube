6799c36e67c95913c54f49c97534c928e1e0bf3e
Toby Qin <toby.qin@live.com>
2017-09-27 10:03:06 +0800

    update model for reset feature.

--- testcube/core/models/reset_result.py
+++ testcube/core/models/reset_result.py
@@ -2,10 +2,10 @@ from datetime import timedelta
 
 from django.db import models
 
-from .test_result import TestResult, ResultError, OUTCOME_CHOICES
+from .test_result import TestResult, ResultError, OUTCOME_CHOICES, TestClient
 
 
-class RerunResult(models.Model):
+class ResetResult(models.Model):
     RERUN_STATUS = ((0, 'None'), (1, 'In Progress'), (2, 'Done'), (3, 'Failed'))
 
     outcome = models.IntegerField(choices=OUTCOME_CHOICES)
@@ -13,15 +13,16 @@ class RerunResult(models.Model):
     duration = models.DurationField(default=timedelta())
     created_on = models.DateTimeField(auto_now_add=True)
 
-    rerun_on = models.DateTimeField(null=True, blank=True)
-    rerun_by = models.CharField(max_length=50, blank=True)
-    rerun_status = models.IntegerField(choices=RERUN_STATUS, default=0)
+    reset_on = models.DateTimeField(null=True, blank=True)
+    reset_by = models.CharField(max_length=50, blank=True)
+    reset_status = models.IntegerField(choices=RERUN_STATUS, default=0)
 
+    test_client = models.ForeignKey(TestClient, on_delete=models.PROTECT, related_name='reset_results')
     error = models.OneToOneField(ResultError, on_delete=models.SET_NULL,
-                                 null=True, blank=True, related_name='result')
+                                 null=True, blank=True, related_name='reset_result')
 
     origin_result = models.ForeignKey(TestResult, on_delete=models.CASCADE,
-                                      related_name='rerun_results')
+                                      related_name='reset_results')
 
     class Meta:
         ordering = ['-created_on']
--- testcube/runner/models.py
+++ testcube/runner/models.py
@@ -3,15 +3,17 @@ from django.db import models
 from testcube.core.models import Product
 
 
-class RunnerProfile(models.Model):
+class Profile(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='runner_profile')
-    command = models.CharField(max_length=200)
+    command = models.CharField(max_length=1000)
 
 
 class Task(models.Model):
     STATUS_CHOICES = ((-1, 'Pending'), (0, 'Sent'))
 
-    name = models.CharField(max_length=100)
+    object_name = models.CharField(max_length=100)
     object_id = models.IntegerField(default=-1)
+    description = models.CharField(max_length=200)
     status = models.IntegerField(choices=STATUS_CHOICES, default=-1)
+    command = models.CharField(max_length=1000)
     data = models.TextField()
