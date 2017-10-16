b872224f9c06ecc89df719bdd8a7466cc2c88671
Toby Qin <toby.qin@live.com>
2017-10-16 17:22:41 +0800

    fix unit tests.

--- testcube/settings.py
+++ testcube/settings.py
@@ -151,6 +151,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter'
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
 
